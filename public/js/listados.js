b7c1daeaeb192ceee876cd46ee37afa348ed5541
jualon92 <londrajuan@gmail.com>
2022-06-03 08:10:20 -0300

    eliminar operacion de db y cliente

--- public/js/listados.js
+++ public/js/listados.js
@@ -1,10 +1,39 @@
-import Lista from "./helpers/Lista.js"
+
 
 console.warn("listado")
+let DATA = []
+const borrar = (id) => {
+    fetch(`/operaciones/${id}`, {
+        method: 'DELETE',
+    })
+        .then(res => res.text()) // or res.json()
+        .then(res => console.log(res))
 
+    console.log(DATA)
+    let dataNueva = DATA.filter(ele => ele.id !== parseInt(id))
+    
 
+    document.querySelector(".tabla-disp").innerHTML = ""
+    dataNueva.forEach(ele => {
 
-document.querySelector(".input-mostrar").addEventListener("change", async (e) => {
+        
+        document.querySelector(".tabla-disp").innerHTML +=
+            `
+        <tr class="table-active">
+            <th scope="row">${ele.concepto}</th>
+            <td>${ele.monto}</td>
+            <td>${ele.fecha}</td>
+            <td>${ele.tipo}</td>
+            <td scope="col"> <button type="button" class="btn-eliminarOp" onclick='borrar(${ele.id})'>X</button> </td>
+        </tr>
+         `
+
+    });
+
+}
+
+document.querySelector(".input-mostrar").addEventListener("click", async (e) => {
+
 
     let valorSeleccionado = document.querySelector(".input-mostrar").value
     console.log("valor sel", valorSeleccionado)
@@ -20,25 +49,38 @@ document.querySelector(".input-mostrar").addEventListener("change", async (e) =>
     let dataFiltrada = data.filter(operacion => operacion.tipo == valorSeleccionado)
     console.log(dataFiltrada)
 
-
+     DATA = dataFiltrada
     //
-    let listaDatos = Lista.normalizar(dataFiltrada)
+
     // document.querySelector(".lista-show").innerHTML = JSON.stringify(listaDatos) // rever
 
 
+    /*
+   let plantillaHbs = await fetch('../templates/tablaListado.hbs').then(r => r.text()) // obtener plantilla
+   var template = Handlebars.compile(plantillaHbs); 
+   let html = template({dataFiltrada}) 
+   document.querySelector(".tabla-disp").innerHTML = html
+   */
 
-    //popular tabla, seria mejor utilizar handlebars o template engine para no mezclar html/logica
+
+    //popular tabla, seria mejor utilizar handlebars o template engine para mantener coloreado
     document.querySelector(".tabla-disp").innerHTML = ""
-    listaDatos.forEach(ele => {
+    dataFiltrada.forEach(ele => {
 
+        
         document.querySelector(".tabla-disp").innerHTML +=
-        `
+            `
         <tr class="table-active">
             <th scope="row">${ele.concepto}</th>
             <td>${ele.monto}</td>
             <td>${ele.fecha}</td>
             <td>${ele.tipo}</td>
+            <td scope="col"> <button type="button" class="btn-eliminarOp" onclick='borrar(${ele.id})'>X</button> </td>
         </tr>
          `
+
     });
+
+
 })
+
