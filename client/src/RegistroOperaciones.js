7438d3eb27f58714650813aec42cc0fe6c903fa8
jualon92 <londrajuan@gmail.com>
2022-06-09 08:14:56 -0300

    input validation registro operacion

--- client/src/RegistroOperaciones.js
+++ client/src/RegistroOperaciones.js
@@ -1,39 +1,55 @@
 import "./RegistroOperaciones.css"
 import {useState} from "react"
 const RegistroOperaciones = () => {
-    const [state, setState] = useState({concepto:"", monto:0,fecha:"",tipo:""})
+    const [state, setState] = useState({concepto:"", monto:"",fecha:"",tipo:"EGRESO"})
+    const [estaDesactivado, setEstaDesactivado] = useState(true)
     
-    
-    const putData = async () => {
-        console.log(state)
-     /*
-        const response = "" 
-      fetch('/operaciones', {
-        method: 'POST', // or 'PUT'
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(operacion),
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-        */
+    const putData = async (e) => {
+      //   e.stopPropagation();
+     //   e.preventDefault()
+        console.log(state.concepto)
+        console.log(state.fecha)
+        if (!hayInputVacio(state)){
+            fetch('/operaciones', {
+                method: 'POST', // or 'PUT'
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(state),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        }else{
+            console.warn("falta input completar")
+        }
+      
+      
+         
   }
 
+  //check if any element is empty
+  const hayInputVacio  = (s) =>  Object.values(s).some( ele => ele == '')
+ 
   const handleChange = (e) => {
     const target = e.target
-    console.log(target)
+    console.log("target", target)
     const name = target.name
-    console.log(name)
-    setState({[name]:e.target.value})
-    console.log(e.target.value)
-
-     
+    console.log("nombre ele", name)
+    setState(prevState => ({
+        ...prevState,
+        [name]: e.target.value
+    }));
+    
+    console.log("value actual", e.target.value)
+    console.log(state)
+    console.log("input vacio presente?: ",hayInputVacio(state))
+    setEstaDesactivado(hayInputVacio(state))   // si hay input vacio esta desactivado
+    
 }
 
 
@@ -41,7 +57,7 @@ const RegistroOperaciones = () => {
     <div className="Registro-contenedor">
       <h1>Registro de operacion </h1>
 
-      <form action="" className="Alta-formulario mb-5">
+      <form  className="Alta-formulario mb-5">
         <label for="concepto">Concepto</label>
         <input
           value={state.concepto}
@@ -54,10 +70,15 @@ const RegistroOperaciones = () => {
         />
 
         <label for="monto">Monto</label>
-        <input type="number" id="monto" name="monto" class="Alta-input" required/>
+        <input type="number" id="monto" name="monto" class="Alta-input"
+         onChange={(e)=>  handleChange(e)}
+         value={state.monto}
+        required/>
 
         <label for="fecha">fecha</label>
         <input
+         value={state.fecha}
+        onChange={(e)=>  handleChange(e)}
           type="date"
           for="fecha"
           id="fecha"
@@ -67,11 +88,12 @@ const RegistroOperaciones = () => {
         />
 
         <label for="tipo">tipo</label>
-        <select id="tipo" name="tipo" class="Alta-input" required>
-          <option value="EGRESO">EGRESO</option>
+        <select id="tipo" name="tipo" class="Alta-input" required   value={state.tipo}
+        onChange={(e)=>  handleChange(e)}>
+          <option value="EGRESO" >EGRESO</option>
           <option value="INGRESO">INGRESO</option>
         </select>
-        <button type="submit" class="Alta-btnAgregar btn btn-success" onClick={putData}>
+        <button type="button" disabled={estaDesactivado} class="Alta-btnAgregar btn btn-success" onClick={ (e) => putData(e)}>
           Agregar Operacion
         </button>
       </form>
